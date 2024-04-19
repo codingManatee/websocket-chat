05e4ca0dd88ae8a50f3ff8adfc19fdffaad9c5f6
WorkingDada <6430222521@student.chula.ac.th>
2024-04-19 10:21:48 +0700

    WIP

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,11 @@ import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001 ;
 
 const __dirname = path.resolve();
 
-dotenv.config();
+dotenv.config({path:'./config/config.env'});
 
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
