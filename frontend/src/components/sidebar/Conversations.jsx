ae7eb88b846b98a56a2c52a147ebe9ade422761a
Maekin <74455990+mekintown@users.noreply.github.com>
2024-04-21 02:16:21 +0700

    Improve ui

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -6,6 +6,7 @@ const Conversations = () => {
 	const { loading, conversations } = useGetConversations();
 	return (
 		<div className='py-2 flex flex-col overflow-auto'>
+			<div className="p-2 font-bold text-gray-200 text-2xl">Rooms</div>
 			{conversations.map((room, idx) => (
 				<Conversation
 					key={room._id}
--- frontend/src/components/sidebar/SidebarChat.jsx
+++ frontend/src/components/sidebar/SidebarChat.jsx
@@ -1,11 +1,8 @@
-import SearchInput from "./SearchInput";
 import Conversations from "./Conversations";
 
 const SidebarChat = () => {
 	return (
-		<div className='border-r border-slate-500 p-4 flex flex-col max-w-[350px]'>
-			<SearchInput />
-			<div className='divider px-3'></div>
+		<div className='border-r border-slate-500 p-4 flex flex-col w-[350px]'>
 			<Conversations/>
 		</div>
 	);
