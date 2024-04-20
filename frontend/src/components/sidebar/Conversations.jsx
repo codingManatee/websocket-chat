2e88b40ce9a677ea29b62a4cbee76a70146d3e57
Maekin <74455990+mekintown@users.noreply.github.com>
2024-04-20 16:30:48 +0700

    fix merge

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,22 @@
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
+import Conversation from "./Conversation";
+
+const Conversations = () => {
+	const { loading, conversations } = useGetConversations();
+	return (
+		<div className='py-2 flex flex-col overflow-auto'>
+			{conversations.map((room, idx) => (
+				<Conversation
+					key={room._id}
+					user={room}
+					emoji={getRandomEmoji()}
+					lastIdx={idx === room.length - 1}
+				/>
+			))}
+
+			{loading ? <span className='loading loading-spinner mx-auto'></span> : null}
+		</div>
+	);
+};
+export default Conversations;
