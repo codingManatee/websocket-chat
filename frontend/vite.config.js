d6516c6bdfe2918ab041e4fc019560649f2a1e11
Maekin <74455990+mekintown@users.noreply.github.com>
2024-04-23 20:00:23 +0700

    wip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8080",
+        target: "http://172.20.10.3:8080",
       },
     },
   },
